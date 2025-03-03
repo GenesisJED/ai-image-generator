ad5b3f80cf5065dbfb5a3867c28edc4833064b70
genesis <escalonagenesis97@gmail.com>
2025-03-03 19:24:21 -0400

    add Setup ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    plugins: {
+      react: pluginReact,
+      "react-hooks": reactHooks,
+      "@next/next": nextPlugin, 
+    },
+    extends: [
+      pluginJs.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      "plugin:react-hooks/recommended",
+      "plugin:@next/next/recommended",
+    ],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "eqeqeq": "error",
+      "indent": ["error", 2],
+      "react/prop-types": "off", 
+      "react/react-in-jsx-scope": "off", 
+      "react-hooks/exhaustive-deps": "warn", 
+      "@next/next/no-img-element": "warn", 
+      "@next/next/no-html-link-for-pages": "error", 
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
-
-export default eslintConfig;
