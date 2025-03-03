3768178b318200c1ec086a8d9f808daa88bde7d6
genesis <escalonagenesis97@gmail.com>
2025-03-03 19:50:18 -0400

    Remove ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import nextPlugin from "@next/eslint-plugin-next";
-import nextConfigRecommended from "@next/eslint-plugin-next/lib/configs/recommended.js";
-import reactHooksRecommended from "eslint-plugin-react-hooks/lib/configs/recommended.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  reactHooksRecommended,
-  ...nextConfigRecommended.configs.recommended,
-  {
-    files: ["**/*.{js,mjs,jsx,tsx}"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-    plugins: {
-      react: pluginReact,
-      "react-hooks": reactHooks,
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "warn",
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"],
-      "eqeqeq": "error",
-      "indent": ["error", 2],
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/exhaustive-deps": "warn",
-      "@next/next/no-img-element": "warn",
-      "@next/next/no-html-link-for-pages": "error",
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
-];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
